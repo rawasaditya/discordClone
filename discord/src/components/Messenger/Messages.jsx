e89dcd4b4267ad07d18deeb19c42c229a06fce84
rawasaditya <rawasaditya@gmail.com>
2023-06-03 15:11:02 +0530

    Clubbed in one date
    Clubbed in one date

--- discord/src/components/Messenger/Messages.jsx
+++ discord/src/components/Messenger/Messages.jsx
@@ -1,12 +1,21 @@
-const Messages = ({ message, author }) => {
+const Messages = ({ message, author, createdAt }) => {
   return (
-    <div className={`chat chat-${!author ? "end" : "start"}`}>
-      <div
-        className={`chat-bubble chat-bubble-${!author ? "accent" : "primary"}`}
-      >
-        {message}
+    <>
+      <div className={`chat chat-${author ? "start" : "end"}`}>
+        <div
+          className={`chat-bubble chat-bubble-${author ? "primary" : "accent"}`}
+        >
+          <span className="">{message}</span>
+        </div>
+        <div
+          className={`w-full text-xs text-${
+            author ? "left" : "right"
+          } opacity-50 chat-footer`}
+        >
+          {createdAt}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- discord/src/components/Messenger/MessengerContainer.jsx
+++ discord/src/components/Messenger/MessengerContainer.jsx
@@ -2,26 +2,53 @@ import { useEffect } from "react";
 import Messages from "./Messages";
 import { connect } from "react-redux";
 import { getDirectChatHistory } from "../../realTimeCommunication/socketConnect.js";
+import { format } from "date-fns";
+
 const MessengerContainer = ({ chosenChatDetails, messages }) => {
   useEffect(() => {
     getDirectChatHistory({
       receiverUserId: chosenChatDetails.id,
     });
   }, [chosenChatDetails]);
+  const clubByDate = () => {
+    let dateClubbedMessages = {};
+    messages.map((i) => {
+      const hasDateIncluded = format(new Date(i.createdAt), "dd/MM/yyyy");
+      if (Object.keys(dateClubbedMessages).includes(hasDateIncluded)) {
+        dateClubbedMessages[hasDateIncluded].push(i);
+      } else {
+        dateClubbedMessages[hasDateIncluded] = [i];
+      }
+    });
+    return dateClubbedMessages;
+  };
+  let clubbedMessages = clubByDate();
   return (
     <div className="h-full py-3 overflow-y-auto">
       <div className="flex-1">
-        <div className="flex flex-col overflow-y-auto align-bottom pb-9 ">
-          {messages.map((i) => {
-            return (
-              <Messages
-                message={i.content}
-                key={i._id}
-                author={i.author._id == chosenChatDetails.id}
-              />
-            );
-          })}
-        </div>
+        {Object.keys(clubbedMessages).map((i) => {
+          return (
+            <>
+              <div className="w-full text-center">
+                <span className="badge badge-neutral">{i}</span>
+              </div>
+              <div className="flex flex-col overflow-y-auto align-bottom pb-9">
+                {clubbedMessages[i].map((j) => {
+                  return (
+                    <>
+                      <Messages
+                        message={j.content}
+                        key={j._id}
+                        author={j.author._id == chosenChatDetails.id}
+                        createdAt={format(new Date(j.createdAt), "HH:MM:SS")}
+                      />
+                    </>
+                  );
+                })}
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
